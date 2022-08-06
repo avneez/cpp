9be3df04ba15a8f43ec3c4413262ccd60a739c3e
Avneez Bhargkeshari <dreamcatcheravneez@gmail.com>
2022-08-07 02:31:26 +0530

    file words count

---
+++ ds in cpp/strings/fileWordsCount.cpp
@@ -0,0 +1,28 @@
+//C++ program to count number of words in text file
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ifstream inFile; //Declares a file stream object
+
+    string fileName;
+    string word;
+    int count = 0;
+
+    cout << "Please enter the file name: ";
+    getline(cin,fileName);
+
+    inFile.open(fileName.c_str());
+
+    while(inFile >> word)
+    {
+        count++;
+    }
+
+    cout << "Number of words in file is: " << count;
+    inFile.close();
+
+    return 0;
+}
